385a33585a14f97fba6f891eb264d0b0ca0a0605
Nguyen Do Thanh Thuy <thuyndt19406c@st.uel.edu.vn>
2022-04-21 01:49:10 +0700

    update header landing, mdbCollapse

--- coude-app/src/app/app.module.ts
+++ coude-app/src/app/app.module.ts
@@ -1,25 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+// import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './header/header.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
 import { LandingComponent } from './landing/landing.component';
-import { RouterModule } from '@angular/router';
 import { WhoWeAreComponent } from './who-we-are/who-we-are.component';
+import { Header1Component } from './header1/header1.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+   HeaderComponent,
     ContactComponent,
     FooterComponent,
     LandingComponent,
     WhoWeAreComponent,
+    Header1Component,
   ],
   imports: [
     BrowserModule,
@@ -27,8 +31,12 @@ import { WhoWeAreComponent } from './who-we-are/who-we-are.component';
      RouterModule.forRoot([
       { path: 'landing', component: LandingComponent },
       { path: 'contact', component: ContactComponent },
+      { path: 'header', component: HeaderComponent },
+      { path: 'header1', component: Header1Component },
       { path: '**', redirectTo: 'landing' },
     ]),
+    MdbCollapseModule,
+    // MdbDropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
