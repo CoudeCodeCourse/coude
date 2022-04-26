2834dac8b2e78f1b1ba4a5a88dba9f69a42d1bac
Nguyen Do Thanh Thuy <thuyndt19406c@st.uel.edu.vn>
2022-04-27 00:41:33 +0700

    thêm favicon, update title cho homepage, sửa res

--- coude-app/src/app/homepage/homepage.component.ts
+++ coude-app/src/app/homepage/homepage.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
+  providers: [Title]
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _title: Title) {
+    this._title.setTitle("Homepage")
+   }
 
   ngOnInit(): void {
   }
