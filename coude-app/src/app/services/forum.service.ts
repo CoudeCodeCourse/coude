373f1ead0f68d146a67c1b778c5a6f566a7745b2
Quynh Le <quynhltd19406c@st.uel.edu.vn>
2022-05-31 11:27:59 +0700

    chỉnh pop-up, tạo topic (chưa thành công)

--- coude-app/src/app/services/forum.service.ts
+++ coude-app/src/app/services/forum.service.ts
@@ -25,7 +25,7 @@ export class ForumService {
 
   //Load data từ server (local 3000)
   uploadData(data: any){
-    return this._http.post(`${url_server}/topics/topics`,data).pipe(
+    return this._http.post(`${url_server}/topics/post-topic`,data).pipe(
       retry(3),
       catchError(this.handleError)
     )
@@ -39,7 +39,7 @@ export class ForumService {
         catchError(this.handleError)
       )
   }
-
+  
   // getAllTopics(){
   //   return this._http.get(`${this.url_server}/topics`).pipe(
   //     retry(3),
